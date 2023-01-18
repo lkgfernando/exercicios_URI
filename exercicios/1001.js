3cd65599dbb1a8e94dbb62c79ce1ff505641ed1a
Fernando José Rodrigues <lkgfernando@gmail.com>
2023-01-18 14:51:48 -0300

    #

---
+++ exercicios/1001.js
@@ -0,0 +1,22 @@
+// Extremamente Básico
+// Adaptado por Neilor Tonin, URI  Brasil
+
+// Timelimit: 1
+// Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Imprima X conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo especificado e não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+
+// Entrada
+// A entrada contém 2 valores inteiros.
+
+// Saída
+// Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da variável X e pelo final de linha. Cuide para que tenha um espaço antes e depois do sinal de igualdade, conforme o exemplo abaixo.
+
+const input = require("fs").readFileSync("stdin", "utf8");
+
+let valores = input.split("\n");
+
+let A = parseInt(valores.shift());
+let B = parseInt(valores.shift());
+
+let X = A + B;
+
+console.log("X = " + X );
