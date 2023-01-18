95551d7951a4a1092be2e79f4002e28eb225a4d9
Fernando José Rodrigues <lkgfernando@gmail.com>
2023-01-18 15:06:28 -0300

    URI 1004

--- exercicios/1004.js
+++ exercicios/1004.js
@@ -11,3 +11,14 @@ O arquivo de entrada contém 2 valores inteiros.
 Saída
 Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem: “Presentation Error”.
 */
+
+const input = require("fs").readFileSync("stdin", "utf8");
+
+let valores = input.split('\n');
+
+let X = parseInt(valores.shift());
+let Y = parseInt(valores.shift());
+
+let prod = X * Y ;
+
+console.log("PROD = " + prod)
--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const input = require("fs").readFileSync("stdin", "utf8");
 
+let valores = input.split('\n');
+
+let X = parseInt(valores.shift());
+let Y = parseInt(valores.shift());
+
+let prod = X * Y ;
+
+console.log("PROD = " + prod)
